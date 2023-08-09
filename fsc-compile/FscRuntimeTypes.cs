45449550273b5c4618d9f71e0707a70f0e537774
Fabian2000 <35495166+Fabian2000@users.noreply.github.com>
2023-08-09 15:47:15 +0200

    Add files via upload

---
+++ fsc-compile/FscRuntimeTypes.cs
@@ -0,0 +1,25 @@
+﻿namespace FSC.Dirty.Runtime.Template
+{
+    /// <summary>
+    /// RuntimeTypes - Dirty comes with some own and new types
+    /// </summary>
+    public enum FscRuntimeTypes
+    {
+        /// <summary>
+        /// Empty return, nothing, no content
+        /// </summary>
+        Void,
+        /// <summary>
+        /// A text is a sequence of chars
+        /// </summary>
+        Text,
+        /// <summary>
+        /// Represents one symbol of a text
+        /// </summary>
+        Char,
+        /// <summary>
+        /// Represents any number like 1, 2, 3.5, 3555,5889, ...
+        /// </summary>
+        Number
+    }
+}
---
+++ fsc.dirty/Program.cs
@@ -0,0 +1,25 @@
+﻿using FSC.Dirty.Runtime;
+
+namespace FSC.Dirty
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            string code = @"
+var result void
+var input text
+extern input from ""ReadLine""
+target here
+extern result from ""WriteLine"", input
+jump here
+";
+
+            DirtyRuntime dirtyRuntime = new DirtyRuntime();
+            dirtyRuntime.AddScript(code);
+            dirtyRuntime.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
